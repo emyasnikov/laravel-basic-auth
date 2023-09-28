5634e61b432f0d4ad571d91b0aab017831224418
Evgenij Myasnikov <evgenij.myasnikov@gmail.com>
2023-09-28 23:41:16 +0200

    Added basic auth config.

---
+++ config/basic_auth.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Basic Authentication
+    |--------------------------------------------------------------------------
+    |
+    | This options are used by the middleware to check if the user has to
+    | authenticate using basic authentication.
+    |
+    */
+
+    'basic_auth' => [
+        'enabled' => env('BASIC_AUTH', false),
+        'password' => env('BASIC_AUTH_PASSWORD', ''),
+        'username' => env('BASIC_AUTH_USERNAME', ''),
+    ],
+
+];
