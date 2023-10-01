2172d6ec9e4f67f0feb75c7a902a190eaf1c3af5
Evgenij Myasnikov <evgenij.myasnikov@gmail.com>
2023-10-01 23:01:06 +0200

    Added orchestra testbench.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,6 +3,7 @@
 namespace Intraset\LaravelBasicAuth\Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Orchestra\Testbench\Concerns\CreatesApplication;
 
 abstract class TestCase extends BaseTestCase
 {
