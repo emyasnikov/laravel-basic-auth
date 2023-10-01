8e615fcb03e1be7781ee02dc158d27333ebde1b5
Evgenij Myasnikov <evgenij.myasnikov@gmail.com>
2023-10-01 18:10:05 +0200

    Added test case.

---
+++ tests/TestCase.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests;
+
+use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+
+abstract class TestCase extends BaseTestCase
+{
+    use CreatesApplication;
+
+    /**
+     * Get package providers.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     * @return array
+     */
+    protected function getPackageProviders($app)
+    {
+        return [
+            'Intraset\LaravelBasicAuth\ServiceProvider',
+        ];
+    }
+}
